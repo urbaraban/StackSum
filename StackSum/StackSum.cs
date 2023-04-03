77d12d1db2c7877ded46472ff81b831e0c2c1f6d
Шадрунов Егор Дмитриевич <urbaraban@outlook.com>
2023-04-03 15:21:28 +0700

    remake to .net6

---
+++ StackSum/StackSum.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.ObjectModel;
+
+namespace StackSumLib
+{
+    public class StackSum : Collection<StackItem>
+    {
+        public float CommonSum
+        {
+            get
+            {
+                float result = 0.0f;
+                for (int i = 0; i < this.Count; i += 1)
+                {
+                    result += this[i].Value;
+                }
+                return result;
+            }
+        }
+        
+    }
+
+    public struct StackItem
+    {
+        public string Display { get; set; }
+        public float Value { get; set; }
+        public int Multiplier { get; set; }
+
+        public StackItem(string display, float value, int multiplier = 1)
+        {
+            this.Display = display;
+            this.Value = value;
+            this.Multiplier = multiplier;
+        }
+    }
+}
